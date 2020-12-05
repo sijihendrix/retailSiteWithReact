3a80ff0770b5f7066ad689fbeb05dc8a0a1d22ff
Siji <sijibamgboye@yahoo.com>
2020-12-05 19:58:15 +0100

    fixed ref issue

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,5 @@
-import { userActionTypes } from "./user.types";
+import { UserActionTypes } from "./user.types";
 export const setCurrentUser = (user) => ({
-  type: userActionTypes.SET_CURRENT_USER,
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
